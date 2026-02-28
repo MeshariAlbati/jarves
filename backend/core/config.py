76ad1369d88996f6afa65e518abd1d587f68e518
Meshari Albati <meshari.albati@gmail.com>
2026-02-28 10:14:23 +0300

    Initial project setup for Jarves

---
+++ backend/core/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    
+    anthropic_api_key: str
+    langsmith_api_key: str
+    langsmith_project: str
+    langsmith_tracing: str
+    telegram_bot_token: str
+    supabase_url: str
+    supabase_key: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8"
+    )
+
+
+settings = Settings()
